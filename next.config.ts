16b571dc91e88da7de48367e68ea35a86cfd2d5e
Diogo Madeira <diogo.madeira@axians.com>
2025-01-13 18:02:48 +0000

    add tanstack query

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        API_BASE_URL: "https://hr-trainings-exp.dali-stage.vinci-energies.net",
+        API_CLIENT_ID: "3a9579cefb934254adef2cd012a51d57",
+        API_CLIENT_SECRET: "3600e8725505485B9D3C923Bc9188Ed6",
+        API_NETWORK_ID: "3"
+    }
+};
 
 export default withNextIntl(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
+import { AppProvider } from "./provider";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import "./globals.css";
@@ -15,6 +21,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const queryClient = new QueryClient();
+
+  // await queryClient.prefetchQuery(getUserQueryOptions());
+
+  const dehydratedState = dehydrate(queryClient);
+
   const locale = await getLocale();
 
   // Providing all messages to the client
@@ -26,15 +38,19 @@ export default async function RootLayout({
       <body
         className={`font-vinci-sans-light antialiased`}
       >
-        <NextIntlClientProvider messages={messages}>
-          <main className='flex min-h-screen flex-col'>
-            <Navbar />
-            <div className='flex-1'>
-              {children}
-            </div>
-            <Footer />
-          </main>
-        </NextIntlClientProvider>
+        <AppProvider>
+          <HydrationBoundary state={dehydratedState}>
+            <NextIntlClientProvider messages={messages}>
+              <main className='flex min-h-screen flex-col'>
+                <Navbar />
+                <div className='flex-1'>
+                  {children}
+                </div>
+                <Footer />
+              </main>
+            </NextIntlClientProvider>
+          </HydrationBoundary>
+        </AppProvider>
       </body>
     </html>
   );

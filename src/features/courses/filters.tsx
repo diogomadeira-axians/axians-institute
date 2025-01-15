d15709c40d9bd4f692e3d8abd64821da171e84d3
ana <ana.paula-kajihara@axians.com>
2025-01-15 17:31:30 +0000

    chore: add input finder

--- src/features/courses/filters.tsx
+++ src/features/courses/filters.tsx
@@ -1,12 +1,15 @@
-import ContactCard from "@/components/contactCard";
-import { Card, CardContent } from "@/components/ui/card";
-import { MdGroup } from "react-icons/md";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function CoursesFilter() {
 
     return (
         <section className="space-y-4 z-10 relative">
-<p>AQUI</p>
+            <div className="flex gap-2">
+                <Input placeholder="Search all courses" />
+                <Button variant="link">Clear</Button>
+                <Button>Search</Button>
+            </div>
 
         </section>
     )
